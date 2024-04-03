bf3b58c18691f9837988e76a8e42aef28d976019
Kyle Abrahamse <kyleabrahamse@outlook.com>
2024-04-03 21:32:56 +0100

    generating recipes for the week successfully

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,64 @@
 "use client";
 
-import { ChangeEvent, useState } from "react";
-import { recipes } from "./recipes";
+import { useState } from "react";
+import { recipes, aisles } from "./recipes";
 
 type RecipeObject = {
   name: string;
   ingredients: string[];
 };
 
-function randomRecipe(arr: RecipeObject[]): RecipeObject {
-  let randomIndex: number = Math.floor(Math.random() * recipes.length);
-  return arr[randomIndex];
-}
-
-// const selectedRecipe = randomRecipe(recipes);
-
 export default function Home() {
-  const [selectedRecipe, setSelectedRecipe] = useState<RecipeObject>();
-  const [num, setNum] = useState<Number>(0);
+  const [weeklyRecipes, setWeeklyRecipes] = useState<RecipeObject[]>([]);
+  const [num, setNum] = useState<number>(0);
 
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    // Ensure value parsed is an number
-    const value: number = parseInt(event.target.value);
-    setNum(value);
+  function randomRecipe(arr: RecipeObject[]): RecipeObject {
+    let randomIndex: number = Math.floor(Math.random() * recipes.length);
+    return arr[randomIndex];
   }
 
-  const generateRandomRecipe = () => {
-    const recipe: RecipeObject = randomRecipe(recipes);
-    setSelectedRecipe(recipe);
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setNum(event.target.valueAsNumber);
+  }
+
+  const generateRandomRecipe = (): void => {
+    let selectedRecipes: RecipeObject[] = [];
+    while (selectedRecipes.length < num) {
+      let recipe = randomRecipe(recipes);
+      if (!selectedRecipes.includes(recipe)) {
+        selectedRecipes.push(recipe);
+      }
+    }
+    setWeeklyRecipes(selectedRecipes);
   };
 
   return (
     <main>
       <h1>Veggie Vibez</h1>
       <h2>Meals</h2>
-      <ul>{selectedRecipe?.name}</ul>
+      <ul>
+        {weeklyRecipes?.map((recipe, i) => {
+          return <li key={i}>{recipe.name}</li>;
+        })}
+      </ul>
       <input
         type="number"
         className="border border-gray-300"
         min="0"
         max="7"
         onChange={handleChange}
-        // value={num}
+        value={num}
       />
       <button onClick={generateRandomRecipe} className="border border-gray-500">
         Genrate Shopping List
       </button>
       <h2>Shopping List</h2>
       <ul>
-        {selectedRecipe?.ingredients.map((recipe, i) => {
-          return <li key={i}>{recipe}</li>;
-        })}
+        {weeklyRecipes?.map((recipe, i) =>
+          recipe.ingredients.map((rec, i) => {
+            return <li key={i}>{rec}</li>;
+          })
+        )}
       </ul>
     </main>
   );
